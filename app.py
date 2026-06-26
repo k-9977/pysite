d5a0af3ea28a25189d6a1ebd16ead9a9d3db24a9
k-9977 <chrollo7799@gmail.com>
2026-06-26 22:34:31 +0530

    added dynamic features

--- app.py
+++ app.py
@@ -1,11 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 
+JOBS = [
+    {
+        'id': 1,
+        'title': 'Palace Archivist',
+        'location': 'Eastern Lotus Wing',
+        'reward': '500 Gold Taels'
+    },
+    {
+        'id': 2,
+        'title': 'Court Musician',
+        'location': 'Moon Pavilion',
+        'reward': '450 Gold Taels'
+    },
+    {
+        'id': 3,
+        'title': 'Lantern Keeper',
+        'location': 'Lantern Courtyard',
+        'reward': '300 Gold Taels'
+    },
+    {
+        'id': 4,
+        'title': 'Palace Librarian',
+        'location': 'Hall of Scrolls',
+        'reward': '500 Gold Taels'
+    }
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
+
+
+@app.route("/jobs")
+def list_jobs():
+    return jsonify(JOBS)
 
 
 if __name__ == "__main__":
